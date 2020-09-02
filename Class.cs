c79834f23648612ec380abb52f828e2547c928af
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-09-02 22:55:25 +0300

    string

--- Class.cs
+++ Class.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp1
             //    string str =  Console.ReadLine();
             //    s = s.Replace(str,str.ToUpper());
             //    Console.WriteLine(s);
-            Console.WriteLine("===================");
+            //Console.WriteLine("===================");
             //Console.Write("ввести строку = ");
             //string s = Console.ReadLine();
             //Console.Write("ввести символ = ");
@@ -24,15 +24,36 @@ namespace ConsoleApp1
             //int ind = -1;
             //for (int i = s.Length - 1; i >= 0; i--)
             //    if (s[i] == c)
-            //    { 
-            //        ind = i; 
-            //        break;
+            //    {
+            //        ind = i;
+            //                           break;
             //    }
             //Console.Write(ind < 0 ? "Символу немає" : $"Строка після перетворення {s.Substring(0, ind)}");
             //Console.ReadKey();
+            //Console.WriteLine("===================");
+            //int count = 0;
+            //Console.Write("ввести строку 1 = ");
+            //string s1 = Console.ReadLine();
+            //Console.Write("ввести строку 2 = ");
+            //string s2 = Console.ReadLine();
 
+            //for (int i = 0; i < s1.Length; i++)
+            //{
+            //    for (int j = 0; j < s2.Length; j++)
+            //    {
+            //        if (s1[i] == s2[j])
+            //        {
+            //            s1 = s1.Remove(i, 1);
 
-
+            //            count++;
+            //            break;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Строка 1: {0}",s1);
+            //Console.WriteLine("Строка 2: {0}", s2);
+            //Console.WriteLine("Всього видалено {0}", count);
+            //Console.WriteLine(s1.Length);
 
         }
     }
