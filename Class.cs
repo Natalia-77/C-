7f0c45e192c11e5418ac87f3b841599556253b98
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-09-05 22:12:17 +0300

    Done

--- Class.cs
+++ Class.cs
@@ -1,164 +1,164 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Linq;
 
-namespace ConsoleApp1
-{
-
-
-    class Programm
-    {
-        static void CoString(string stroka)
-        {
-            int kol = 0;//лічильник кількості літер в строці.
-            int koldigit = 0, kolprep = 0;
-            int pkol = 0;
-            string punkt = ".,!?";
-            string probel = " ";
-
-            Console.WriteLine("Кількість символів в строці :{0}", stroka.Length);
-            Console.WriteLine("===============================");
-            foreach (var el in stroka)
-            {
-                if (char.IsLetter(el))
-                    kol++;
-                if (char.IsDigit(el))
-                    koldigit++;
-                foreach (char punct in punkt)
-                    if (el == punct)
-                        kolprep++;
-                foreach (char p in probel)
-                    if (el == p)
-                        pkol++;
+//namespace ConsoleApp1
+//{
 
 
-            }
-            Console.WriteLine("Кількість літер в строці :{0}", kol);
-            Console.WriteLine("Кількість літер у нижньомі регістрі : " + stroka.Count(Char.IsLower));
-            Console.WriteLine("Кількість літер у верхньому регістрі : " + stroka.Count(Char.IsUpper));
-            Console.WriteLine("Кількість цифр у строці:{0}", koldigit);
-            Console.WriteLine("Кількість знаків пунктуації {0}", kolprep);
-            Console.WriteLine("Кількість пробілів {0}", pkol);
+//    class Programm
+//    {
+//        static void CoString(string stroka)
+//        {
+//            int kol = 0;//лічильник кількості літер в строці.
+//            int koldigit = 0, kolprep = 0;
+//            int pkol = 0;
+//            string punkt = ".,!?";
+//            string probel = " ";
+
+//            Console.WriteLine("Кількість символів в строці :{0}", stroka.Length);
+//            Console.WriteLine("===============================");
+//            foreach (var el in stroka)
+//            {
+//                if (char.IsLetter(el))
+//                    kol++;
+//                if (char.IsDigit(el))
+//                    koldigit++;
+//                foreach (char punct in punkt)
+//                    if (el == punct)
+//                        kolprep++;
+//                foreach (char p in probel)
+//                    if (el == p)
+//                        pkol++;
 
 
+//            }
+//            Console.WriteLine("Кількість літер в строці :{0}", kol);
+//            Console.WriteLine("Кількість літер у нижньомі регістрі : " + stroka.Count(Char.IsLower));
+//            Console.WriteLine("Кількість літер у верхньому регістрі : " + stroka.Count(Char.IsUpper));
+//            Console.WriteLine("Кількість цифр у строці:{0}", koldigit);
+//            Console.WriteLine("Кількість знаків пунктуації {0}", kolprep);
+//            Console.WriteLine("Кількість пробілів {0}", pkol);
 
 
 
-        }
-
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
 
 
+//        }
 
-             //------------------#1------------------
-             // Користувач вводить рядок і символ. 
-             // У рядку знайти всі входження цього символа і перевести його у верхній регістр, 
-             // а також видалити частину рядка, після останнього входження цього символу і до кінця.
-            Console.WriteLine("ввести строку =");
-            string s = Console.ReadLine();
-            Console.WriteLine("ввести символ =");
-            string str = Console.ReadLine();
-            s = s.Replace(str, str.ToUpper());
-            Console.WriteLine(s);
-            Console.WriteLine("===================");
+//        static void Main(string[] args)
+//        {
+//            Console.OutputEncoding = Encoding.Unicode;
+//            Console.InputEncoding = Encoding.Unicode;
 
 
-            int find = s.LastIndexOf(str, s.Length - 1, StringComparison.CurrentCultureIgnoreCase);//шукає індекс входження підстроки в строку.
-            Console.Write(find);//вивела в консоль індекс(просто для перевірки)
 
-            s = s.Remove(find + 1);
-            Console.WriteLine(s);
-            Console.WriteLine("===================");
-
-
-            //-----------------------#2----------------
-            // Дано два рядки. 
-            //Необхідно з першого рядка видалити всі символи, які зустрічаються в рядку 2.
-            //Вивести одержаний рядок, 
-            //а також статистику по обробці:
-            //кількість видалених символів усього
-            //кількість видалених входжень кожного символа
-            //кількість символів, яка залишилась в рядку.
-
-            Console.Write("ввести строку 1 = ");
-            string s1 = Console.ReadLine();
-            Console.Write("ввести строку 2 = ");
-            string s2 = Console.ReadLine();
-
-            int count = 0,//рахує кількість видалених літер.
-            len = s2.Length;
-            string s3 = "";//створила додаткову строчку,куди буду записувати видалені літери.
-
-            for (int i = 0; i < s1.Length; i++)
-            {
-
-                for (int j = 0; j < s2.Length; j++)
-                {
-                    if (s1[i] == s2[j])
-                    {
-                        if (s2.Contains(s1[i]))
-                        {
-
-                            s3 += s1[i];//ось тут записую в новостворену строчку значення видалених символів.
-
-                        }
-
-                        s1 = s1.Remove(i, 1);
-                        count++;
-                        break;
-
-                    }
-                }
-            }
-
-            Console.WriteLine("Строка 1: {0}", s1);
-            Console.WriteLine("Строка 2: {0}", s2);
-            Console.WriteLine("Строка 3: {0}", s3);
-            Console.WriteLine("Всього видалено {0}", count);
-            Console.WriteLine("Довжина Строки 1 після видалення:{0} ", s1.Length);
-
-            int kol = 0; //кількість літер.
-            char pred = ' '; //попередній символ.
-
-            foreach (char ch in s3) //тут кожен окремий символ беру за основу для порівняння.
-            {
-                foreach (char cha in s3) //іду по всім символам строки.
-
-                    if (cha != pred && cha == ch) //поточний символ має бути рівним символу-основі і не бути рівним попередньому символу-основі.
-                        kol++; //кількість літер.
-
-                if (kol != 0) //якщо не рівно нулю,то виводимо в консоль результат.
-                    Console.WriteLine("Кількість літер  {0} = {1}", ch, kol); //виводжу в консоль символ і його кількість в цій строці.
-
-                kol = 0; //обнуляю,для пошуку наступної кількості літер.
-                pred = ch; //Переходжу на наступну літеру(яку беру за основу для порівнянн) строки.
-
-            }
+//             //------------------#1------------------
+//             // Користувач вводить рядок і символ. 
+//             // У рядку знайти всі входження цього символа і перевести його у верхній регістр, 
+//             // а також видалити частину рядка, після останнього входження цього символу і до кінця.
+//            Console.WriteLine("ввести строку =");
+//            string s = Console.ReadLine();
+//            Console.WriteLine("ввести символ =");
+//            string str = Console.ReadLine();
+//            s = s.Replace(str, str.ToUpper());
+//            Console.WriteLine(s);
+//            Console.WriteLine("===================");
 
 
-            //----------------#3-------------
-            //
-            //Створити метод, що приймає введений користувачем рядок і виводить на екран статистику по цьому рядку. 
-            //Статистика повинна включати:
-            // загальну кількість символів,
-            //кількість літер
-            //скільки літер у верхньому регістрі
-            //скільки літер у нижньому регістрі
-            //кількість цифр,
-            //кількість символів пунктуації
-            //кількість пробільних символів.
+//            int find = s.LastIndexOf(str, s.Length - 1, StringComparison.CurrentCultureIgnoreCase);//шукає індекс входження підстроки в строку.
+//            Console.Write(find);//вивела в консоль індекс(просто для перевірки)
+
+//            s = s.Remove(find + 1);
+//            Console.WriteLine(s);
+//            Console.WriteLine("===================");
 
 
-            Console.WriteLine("ввести строку =");
-            string st = Console.ReadLine();
-            Console.WriteLine();
-            CoString(st);
-            Console.WriteLine("===================");
+//            //-----------------------#2----------------
+//            // Дано два рядки. 
+//            //Необхідно з першого рядка видалити всі символи, які зустрічаються в рядку 2.
+//            //Вивести одержаний рядок, 
+//            //а також статистику по обробці:
+//            //кількість видалених символів усього
+//            //кількість видалених входжень кожного символа
+//            //кількість символів, яка залишилась в рядку.
 
-        }
-    }
-}
+//            Console.Write("ввести строку 1 = ");
+//            string s1 = Console.ReadLine();
+//            Console.Write("ввести строку 2 = ");
+//            string s2 = Console.ReadLine();
+
+//            int count = 0,//рахує кількість видалених літер.
+//            len = s2.Length;
+//            string s3 = "";//створила додаткову строчку,куди буду записувати видалені літери.
+
+//            for (int i = 0; i < s1.Length; i++)
+//            {
+
+//                for (int j = 0; j < s2.Length; j++)
+//                {
+//                    if (s1[i] == s2[j])
+//                    {
+//                        if (s2.Contains(s1[i]))
+//                        {
+
+//                            s3 += s1[i];//ось тут записую в новостворену строчку значення видалених символів.
+
+//                        }
+
+//                        s1 = s1.Remove(i, 1);
+//                        count++;
+//                        break;
+
+//                    }
+//                }
+//            }
+
+//            Console.WriteLine("Строка 1: {0}", s1);
+//            Console.WriteLine("Строка 2: {0}", s2);
+//            Console.WriteLine("Строка 3: {0}", s3);
+//            Console.WriteLine("Всього видалено {0}", count);
+//            Console.WriteLine("Довжина Строки 1 після видалення:{0} ", s1.Length);
+
+//            int kol = 0; //кількість літер.
+//            char pred = ' '; //попередній символ.
+
+//            foreach (char ch in s3) //тут кожен окремий символ беру за основу для порівняння.
+//            {
+//                foreach (char cha in s3) //іду по всім символам строки.
+
+//                    if (cha != pred && cha == ch) //поточний символ має бути рівним символу-основі і не бути рівним попередньому символу-основі.
+//                        kol++; //кількість літер.
+
+//                if (kol != 0) //якщо не рівно нулю,то виводимо в консоль результат.
+//                    Console.WriteLine("Кількість літер  {0} = {1}", ch, kol); //виводжу в консоль символ і його кількість в цій строці.
+
+//                kol = 0; //обнуляю,для пошуку наступної кількості літер.
+//                pred = ch; //Переходжу на наступну літеру(яку беру за основу для порівнянн) строки.
+
+//            }
+
+
+//            //----------------#3-------------
+//            //
+//            //Створити метод, що приймає введений користувачем рядок і виводить на екран статистику по цьому рядку. 
+//            //Статистика повинна включати:
+//            // загальну кількість символів,
+//            //кількість літер
+//            //скільки літер у верхньому регістрі
+//            //скільки літер у нижньому регістрі
+//            //кількість цифр,
+//            //кількість символів пунктуації
+//            //кількість пробільних символів.
+
+
+//            Console.WriteLine("ввести строку =");
+//            string st = Console.ReadLine();
+//            Console.WriteLine();
+//            CoString(st);
+//            Console.WriteLine("===================");
+
+//        }
+//    }
+//}
--- Mas1.cs
+++ Mas1.cs
@@ -1,57 +1,249 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace ConsoleApp1
-//{
-//    class Mas1
-//    {
-//        static void Main(string[] args)
-//        {
-//            int count = 0;
-//            int[] array1 = { 5, 4, 0, 0, 1, 0, 4, 0, 9 };
-//            int[] array2 = new int[array1.Length];
+namespace ConsoleApp1
+{
+    class Mas1
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
 
-//            Console.WriteLine("Початковий масив значень : ");
-//            Console.WriteLine();
+            //Сжать массив, удалив из него все 0 и, заполнить ос-
+            //вободившиеся справа элементы значениями –1
+
+
+            int count = 0;
+            int[] array = { 5, 4, 0, 0, 1, 0, 4, 0, 9 };
+
+
+            Console.WriteLine("Початковий масив значень : ");
+            //Console.WriteLine();
+
+            foreach (int el in array)
+            {
+                Console.Write(el + " ");
+            }
+
+            Console.WriteLine();
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 0)
+                    count++;
+                else
+                    array[i - count] = array[i];
+            }
+            for (int i = array.Length - count; i < array.Length; i++)
+            {
+                array[i] = -1;
+            }
+
+
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+
+
+            //Преобразовать массив так, чтобы сначала шли все 
+            //отрицательные элементы, а потом положительные
+            //(0 считать положительным)
+
+            // Перетворений і відсортований масив
+
+            int kol = 0;
+            int[] array1 = { -5, -10, 0, 2, 1, 14, -2, -1, 5 };
+            int[] temp = new int[array1.Length];
+            Console.WriteLine();
+            Console.WriteLine("Початковий масив значень : ");
            
-//            foreach (int el in array1)
-//            {
-//                Console.Write(el+" ");
-//            }
 
-//            Console.WriteLine();
+            foreach (int el in array1)
+            {
+                Console.Write(el + " ");
+            }
 
-//            for (int i = 0; i < array1.Length; i++)
-//            {
-//                if (array1[i] != 0)
-//                {
-//                    array2[count] = array1[i];
-//                    count++;
-//                }
-//            }
-//            for (int i = count; i < array1.Length; i++)
-//            {
-//                array2[i] = -1;
-//            }
-                    
+            //Console.WriteLine();
+            for (int i = 0; i < array1.Length - 1; ++i)
+                for (int j = i + 1; j < array1.Length; ++j)
+                    if (array1[i] > array1[j])
+                    {
+                        temp[i] = array1[i];
+                        array1[i] = array1[j];
+                        array1[j] = temp[i];
+                    }
 
-//            Console.WriteLine();
-//            Console.WriteLine("Новий масив значень : ");
-//            Console.WriteLine();
-          
 
-//            foreach (int item in array2)
-//            {
-//                Console.Write(item + " ");
-//            }
+            foreach (int el in array1)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
 
-//            Console.WriteLine();
+            //Перетворений і НЕ відсортований масив.
 
-//            Console.ReadLine();
-//        }
-//    }
-//}
+            int k = 0;
+            int[] array2 = { -5, -4, 0, 21, 12, 4, -2, -1, 5 };
+            int[] array3 = new int[array2.Length];
+
+            Console.WriteLine();
+            Console.WriteLine("Початковий масив значень :");           
+
+            foreach (int el in array2)
+            {
+                Console.Write(el + " ");
+            }
+
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array2[i] < 0)
+                {
+                    array3[k] = array2[i];
+                    k++;
+                }
+
+            }
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array2[i] >= 0)
+                {
+                    array3[k] = array2[i];
+                    k++;
+                }
+            }
+
+            array3.CopyTo(array2, 0);
+            Console.WriteLine();
+            foreach (int el in array2)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+
+
+            //Написать программу, которая предлагает пользова-
+            //телю ввести число и считает, сколько раз это число
+            //встречается в массиве.
+
+
+
+            var rnd = new Random();
+            var array4 = new int[25];
+            for (int i = 0; i < array4.Length; i++)
+            {
+                array4[i] = rnd.Next(0, 11);
+            }
+            int number, kolvo = 0;
+            Console.WriteLine();
+            Console.WriteLine("Рандомно сформований масив:");
+            foreach (int el in array4)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Введіть число від 0 до 10: \n");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < array4.Length; i++)
+            {
+                if (array4[i] == number)
+                {
+                    kolvo++;
+                }
+            }
+            Console.WriteLine($"Кількість числа {number} в масиві становить {kolvo} \n");
+            Console.WriteLine("========================================================\n");
+
+
+            //В двумерном массиве порядка M на N поменяйте 
+            //местами заданные столбцы.
+
+            int[,] array6 = new int[7, 7];
+            var random = new Random();
+            int col, col2,temp_t,row,row2 = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    array6[i, j] = random.Next(100);
+                    Console.Write("{0,6}", array6[i, j]);
+                   
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Введіть номер першої колонки");
+            col = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть номер другої колонки");
+            col2 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (j == col - 1)
+                    {
+                        temp_t = array6[i, j];
+                        array6[i, j] = array6[i, col2 - 1];
+                        array6[i, col2 - 1] = temp_t;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                   
+                    Console.Write("{0,6}", array6[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Ви можете поміняти місцями і рядочки\n");
+            Console.WriteLine("Введіть номер першого рядочка");
+            row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть номер другого рядочка");
+            row2 = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i == row - 1)
+                    {
+                        temp_t = array6[i, j];
+                        array6[i, j] = array6[row2-1,j];
+                        array6[row2-1,j] = temp_t;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+
+                    Console.Write("{0,6}", array6[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
 
 
 
