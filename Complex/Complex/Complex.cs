0b22925f87b4f510b37e69ccf1fd598ee352ae6b
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-09-22 22:49:03 +0300

    Done

---
+++ Complex/Complex/Complex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complex
+{
+    class Complex
+    {
+        private int _a { get; set; }
+        private int _b { get; set; }
+
+        public Complex(int a,int b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Дійсне число {0} уявна одиниця {1}", _a, _b);
+        }
+        public static Complex operator +(Complex x, Complex y)
+            => new Complex(x._a + y._a, x._b + y._b);
+
+        public static Complex operator -(Complex x, Complex y)
+            => new Complex(x._a - y._a, x._b - y._b);
+
+       public static Complex operator *(Complex x, Complex y)
+            => new Complex(x._a * y._a, x._b * y._b);
+
+        public static Complex operator /(Complex x, Complex y)
+            => new Complex(x._a / y._a, x._b / y._b);
+
+        public static Complex operator -(Complex x, int y)
+            => new Complex(x._a - y, x._b - y);
+
+        public static Complex operator *(int x, Complex y)
+            => new Complex(x * y._a, x * y._b);
+
+        public static Complex operator *(Complex x, int a)
+        => new Complex(x._a * a, x._b * a);
+       
+
+
+
+    }
+}
---
+++ Complex/Complex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Complex z = new Complex(2, 3);
+            Complex z1;
+            z1 = z - (z * z * z - 1) / (3 * z * z);     
+            //-----просто перевірки-----
+            // z1 = z * z * z;
+            //z1 = z * z * z - 1;
+            //z1 = z - (z * z * z - 1);
+            //z1 = 3 * z * z;            
+            Console.WriteLine(" Результат z1 = {0}", z1);
+        }
+    }
+}
