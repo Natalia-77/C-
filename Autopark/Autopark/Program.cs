22d67c73d2aff9c09118523104383e01bb3541dc
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-10-02 09:45:52 +0300

    .dll

---
+++ Autopark/Autopark/Program.cs
@@ -0,0 +1,18 @@
+﻿using Parking;
+using System;
+using System.Text;
+
+namespace Autopark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Menu menu = new Menu();
+            menu.MainMenu();
+        }
+    }
+}
