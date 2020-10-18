9cfbf52a9f74bb525ecc468021c4788d5f3c6020
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-10-18 18:23:19 +0300

    Tamagochu

---
+++ Forms/Forms/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Timers;
+
+
+
+namespace Tamagochi
+{
+    class Program
+    {
+        public static void Tami(object obj, EventArgs e)
+        {
+
+            Tama t = new Tama("Lolo", "Yellow");
+            List<Moves> m = new List<Moves>(new Moves[] { t.Sleep, t.Eat, t.Play, t.Walk, t.Drink, t.Wash, t.Makeup });
+
+            Random rand = new Random();
+            Random r = new Random((int)DateTime.Now.Ticks);
+            int counter = 0;
+            // Новий ліст,куди додаю нові згeнеровані методи делегата.
+            List<Moves> mov = new List<Moves>();
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("| Wellcome to Tamagochi |");
+            Console.WriteLine("-------------------------");
+
+            while (m.Count > 0)
+            {
+                // Два різні рандоми для генерації індексів.
+                int index = rand.Next(m.Count);
+                int ind = r.Next(m.Count);
+
+                mov.Add(m[index]);
+
+
+                for (int i = 0; i < mov.Count; i++)
+                {
+
+                    for (int j = i + 1; j < mov.Count; j++)
+                    {
+
+                        if (mov[j].Method.Name == mov[i].Method.Name)
+                        {
+                            // Перевіряла,чи робить заміну на новий метод,якщо повтор.
+                            //Console.WriteLine("+++++");
+                            //Console.WriteLine(mov[j].Method.Name);
+
+                            // Якщо є повтор,то видаляю.
+                            mov.RemoveAt(i);
+
+                            // Потім на це видалене місце додаю новий,але  індекс згенеровано новим рандомом.
+                            mov.Add(m[ind]);
+                        }
+
+                    }
+
+                }
+
+                foreach (Moves item in mov)
+                {
+                    // Викликаю.
+                    item.DynamicInvoke();
+
+                    //Console.WriteLine("----------------------------------------------------------------");
+                    //Console.WriteLine("Do you want to satisfy a need?");
+                    //Console.WriteLine("Enter '+' if your answer 'YES' or enter '-' if your answer 'NO'\n");
+                    //Console.WriteLine("-----------------------------------------------------------------");
+
+                    ConsoleKeyInfo keys = Console.ReadKey();
+                    if (keys.KeyChar == '+')
+                    {
+                        Console.WriteLine("Super\n");
+
+                    }
+                    else if (keys.KeyChar == '-')
+                    {
+                        Console.WriteLine("Bad\n");
+                        counter++;
+                        Console.WriteLine("You refuse to tami " + counter);
+                        Console.WriteLine("---------------");
+
+                        // Якщо тричі відмовити,то просить полікувати.
+                        if (counter == 3)
+                        {
+                            t.Treat();
+
+                            Console.WriteLine("Do you want treat me?");
+                            Console.WriteLine("Enter '1' if your answer 'YES' or enter '0' if your answer 'NO'");
+                            ConsoleKeyInfo choice = Console.ReadKey();
+                            if (choice.KeyChar == '1')
+                            {
+                                Console.WriteLine("Thank you!");
+                                Console.Clear();
+                                counter = 0;
+                            }
+                            else if (choice.KeyChar == '0')
+                            {
+                                Console.Clear();
+                                t.Die();
+                                Console.WriteLine();
+                                return;
+                            }
+
+                        }
+
+                    }
+                    //Console.Clear();
+
+                }
+            }
+
+        }
+    
+
+        private delegate void Moves();
+
+
+        private static System.Timers.Timer timer;
+        
+
+        static void Main(string[] args)
+        {
+
+            SetTimer();
+
+            Console.ReadLine();
+            timer.Stop();
+            timer.Dispose();
+
+
+        }
+        private static void SetTimer()
+        {
+            // Інтервал в 3 сек.
+            timer = new System.Timers.Timer(3000);           
+            timer.Elapsed += Tami;
+            //timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+
+
+    }
+}
+
+
+
+
+        
+        
+    
+
+
+
+        
+
+    
+
+
+    
+
+
+
+
+      
+
---
+++ Forms/Forms/Tama.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagochi
+{
+    class Tama
+    {
+
+        private  string name { get; set; }
+        private string color { get; set; }
+
+        public Tama(string name,string color)
+        {
+            this.name = name;
+            this.color = color;
+        }        
+
+        public  void Walk()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Walk with me!");
+           
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Sleep with me!");
+          
+        }
+
+        public  void Eat()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Eat with me!");
+            
+        }
+
+        // Прохання полікувати.
+        public  void Treat()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Treated me!Please!");
+          
+        }
+
+        public  void Play()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Play with me!");
+          
+        }
+
+        public void Wash()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Wash me with shampoo !");
+        }
+
+        public void Drink()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("I want to Drink !");
+        }
+
+        public void Makeup()
+        {
+            Console.WriteLine(color + " " + name + " says ");
+            Console.WriteLine("Would you like to get me makeup!");
+        }
+        public void Die()
+        {
+            Console.WriteLine("I'm die(((((");
+        }
+
+
+      
+
+
+
+
+
+
+    }
+}
