5100898be5552d64b365f215b058e8b15c05ab0e
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-10-30 12:19:20 +0200

    Done

--- Filestream/Filestream/Program.cs
+++ Filestream/Filestream/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Filestream
 {
@@ -8,7 +9,73 @@ namespace Filestream
     {
         static void Main(string[] args)
         {
-            string name = "D:/Natalia/Project1/C-Sharp/Filestream/Filestream/images.jpg";
+
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            //string name = "D:/Natalia/Project1/C-Sharp/Filestream/Filestream/images.jpg";
+
+            string name1 = string.Empty;
+           
+            // Залишила лише,щоб тільки диск Д приймало.
+            string pattern = "[(D)]";
+            var r = new Regex(pattern);
+
+            bool flag = false;
+            do
+            {
+
+                Console.WriteLine("Введіть шлях до файлу(наприклад D:--коса риска--   :");
+                 name1 = Console.ReadLine();
+
+
+                if (r.IsMatch(name1))
+                {                   
+                    Console.WriteLine("Супер!Поки що все вірно!");
+                    flag = true;
+                }
+                else
+                {
+                     Console.WriteLine("Неправильно введений шлях.Перевірте дані.");                   
+                }
+
+            } while (!flag);
+
+           
+
+            // Тут регулярним виразом перевіряю наявність вказаного розширення.
+            var r2 = new Regex(@".jpg(\w*)");
+            string name2 = string.Empty;
+            bool flag2 = false;
+            do
+            {
+                Console.WriteLine("Введіть ім1я файлу.Не забудьте про розширення .jpg");
+                name2 = Console.ReadLine();
+
+                if (r2.IsMatch(name2))
+                {
+                    Console.WriteLine("Супер!Все вірно!");
+                    flag2 = true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Неправильно введене ім1я файлу.");
+                    
+                }
+
+            } while (!flag2);
+
+            string name = name1 + name2;
+            if(File.Exists(name))
+            {
+                Console.WriteLine("Введений вами файл існує");
+            }
+            else
+            {
+                Console.WriteLine("Такого файлу ,принамі у нас,немає. ");
+            }
+
             byte[] bytes;
             int len = 0;
 
