3f398152ad43e475912249c310086735a207955e
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-11-06 15:31:32 +0200

    First commit

---
+++ FileManager/FileManager/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;           
+            Table table = new Table();          
+            table.Tables();
+
+
+
+        }
+
+    }
+}
+    
+
---
+++ FileManager/FileManager/Table.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileManager
+{
+    class Table
+    {
+        public void Tables()
+        {
+            
+            string dirName = "D:\\";
+            if (System.IO.Directory.Exists(dirName))
+            {
+
+                int count = 0;
+                string[] dirs = Directory.GetDirectories(dirName);
+               
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"╔═════════════════════{dirName}══════════════════════╗");
+                Console.WriteLine("║     Name        │  Size         │  Date      ║");
+
+                foreach (var s in dirs)
+                {
+                    if (s.Length > 10)
+                    {
+                        Console.WriteLine($"║{s.Substring(3, 8) + (char)16,-10}       │               │ {new FileInfo(dirName).CreationTime}           ║");
+                        count++;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"║{s.Substring(3),-10}       │               │            ║");
+                        count++;
+                    }
+
+
+                }
+                if (count < 20)
+                {
+                    for (int i = 0; i < 20 - count; i++)
+                    {
+                        Console.WriteLine("║                 │               │            ║");
+                    }
+                    Console.WriteLine("║─────────────────┼───────────────┼────────────║");
+                }
+
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
