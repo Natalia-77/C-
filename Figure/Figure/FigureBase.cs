ebb2ca6e0f1fb4f164bb284392cac2004a9e9fc9
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-01-12 16:42:41 +0200

    first commit

---
+++ Figure/Figure/FigureBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Figure
+{
+    abstract class FigureBase
+    {
+        string name{ get; set; }
+        string color { get; set; }       
+
+        public FigureBase(string name,string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+
+        public abstract double Area();
+       // public abstract string Color();
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Figure name :{name} color :{color}");
+        }
+        
+    }
+
+    
+
+}
---
+++ Figure/Figure/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    class Triangle : FigureBase
+    {
+        /// <summary>
+        /// Triangle sides.
+        /// </summary>
+        double a { get; set; }
+        double b { get; set; }
+        double c { get; set; }
+
+        public Triangle(string name, string color, double a, double b, double c) : base(name, color)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double Area()
+        {
+            double perim = (a + b + c) / 2;
+            return Math.Round(Math.Sqrt(perim*(perim-a)*(perim-b)*(perim-c)), 1);
+        }
+    }
+}
+
+
